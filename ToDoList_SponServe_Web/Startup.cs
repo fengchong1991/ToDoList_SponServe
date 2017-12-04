4eb33628642e5181a3c4551e0bd6a77373bee537
Chong Feng <fengchong1991@gmail.com>
2017-12-04 18:50:24 +1100

    Add project files.

---
+++ ToDoList_SponServe_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToDoList_SponServe_Web.Startup))]
+namespace ToDoList_SponServe_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
